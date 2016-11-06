8596b752d4b649f23c7b432a1b8c48d9432a1edb
jfchagaris <johnchagaris@Johns-MBP-3.home>
2016-11-06 16:43:17 -0500

    removed failed attempt comments

--- function.py
+++ function.py
@@ -1,14 +1,11 @@
 def add_to_list(a_list,new_item):
 	
 	if new_item in a_list:
-		#pos = a_list.index(new_item)
-		#a_list[pos] = new_item
 		return False
+	
 	else:
-		#a_newer_list = 
 		a_list.append(new_item)
 		print(a_list)
-		#print(a_newer_list)
 
 my_list = ['tree','flower','a','b','c','d']
 
